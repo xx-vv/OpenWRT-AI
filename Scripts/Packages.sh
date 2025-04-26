18da8b54f54ee1c15e26bbb8a5a0a3a4ed80a10c
xx-vv <135017778+xx-vv@users.noreply.github.com>
2025-04-26 12:00:08 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -65,7 +65,7 @@ UPDATE_PACKAGE "viking" "VIKINGYFY/packages" "main" "" "luci-app-timewol luci-ap
 UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 
 UPDATE_PACKAGE "modemdefine" "4IceG/luci-app-modemdefine" "main"
-UPDATE_PACKAGE "adguardhome" "xiaoxiao29/luci-app-adguardhome" "master" "pkg"
+UPDATE_PACKAGE "adguardhome" "xx-vv/adguardhome" "main"
 UPDATE_PACKAGE "luci-app-adguardhome" "sirpdboy/luci-app-adguardhome" "js"
 UPDATE_PACKAGE "lucky" "kenzok8/openwrt-packages" "master" "pkg"
 UPDATE_PACKAGE "luci-app-advanced" "kenzok8/openwrt-packages" "master" "pkg"
